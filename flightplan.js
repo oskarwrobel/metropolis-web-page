5e877c5d845c4c8b30807acd7a55060c58cfea03
Oskar Wrobel <kontakt@oskarwrobel.pl>
2015-11-26 20:21:35 +0100

    Add FB and twitter SDK + flightplan

--- flightplan.js
+++ flightplan.js
@@ -2,11 +2,8 @@
 
 var plan = require('flightplan');
 var path = require('path');
-var config = require('./config/deploy');
 
-'/var/www/metropolis/'
-
-var appPath = '/var/www/joannalawniczak.com';
+var appPath = '/var/www/metropolis';
 var appDirName = appPath.split('/').pop();
 var tmpDirName = +new Date() + '/';
 var tmpDir = appDirName + '-' + tmpDirName;
@@ -21,27 +18,23 @@ plan.target('production', [{
 plan.local('deploy', function(local) {
     local.log('Run build');
     local.exec('npm install');
+    local.exec('bower install');
     local.exec('gulp');
 
     local.log('Copy files to remote host');
-    var filesToCopy = local.exec('(git ls-files -z;find public/dist -type f -print0)', {silent: true});
+    var filesToCopy = local.exec('(find dist -type f -print0)', {silent: true});
     local.transfer(filesToCopy, '/tmp/' + tmpDir);
 });
 
 plan.remote('deploy', function(remote) {
-    remote.log('Move folder to web root');
+    remote.log('Create app root directory if not exists');
     remote.mkdir('-p ' + appPath);
-    remote.cp('-R ' + path.join('/tmp/', tmpDir, ' ', appPath, tmpDirName));
+
+    remote.log('Clear previous version');
+    remote.rm('-rf ' + path.join(appPath, '*'));
+
+    remote.log('Move folder to web root');
+    remote.mkdir('-p ' + path.join(appPath, 'projects', 'metropolis'));
+    remote.cp('-r ' + path.join('/tmp/', tmpDir, 'dist/* ', appPath, 'projects', 'metropolis/'));
     remote.rm('-rf ' + path.join('/tmp/', tmpDir));
-
-    remote.log('Install dependencies');
-    remote.exec('npm install --production  --prefix ' + path.join(appPath, tmpDirName));
-
-    remote.log('Reload application');
-    remote.ln('-sfn ' + path.join(appPath, tmpDirName, ' ', appPath, 'current'));
-    remote.exec('forever stop ' + appPath + serverFile, {failsafe: true});
-    remote.exec('NODE_ENV=production forever start ' + appPath + serverFile);
-
-    remote.log('Remove old version');
-    remote.find(appPath + ' -maxdepth 1 | tail -n +2 | sort -r | tail -n +4 | xargs rm -rf');
 });
